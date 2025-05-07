7d5c07d172810aedde827972c9cde4ad5ec0711c
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-05-07 23:48:56 +0300

    created an Exchange class that handles getting money to be exchanged and rate and calculating the right amount of money

--- CurrencyExchangeApp.java
+++ CurrencyExchangeApp.java
@@ -1,3 +1,29 @@
-public class CurrencyExchangeApp {
+import java.util.*;
 
+public class CurrencyExchangeApp {
+    public static void main(String[] args){
+        System.out.println(Exchange.calculateExchange());
+    }
+}
+
+class Exchange {
+    
+    public static float moneyToBeExchanged() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("please enter the amount of money you want to exchange: ");
+        float money = sc.nextFloat();
+        return money;
+    }
+        
+    public static float getRate() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("please enter the current exchange rate: ");
+        Float rate = sc.nextFloat();
+        return rate;
+    }
+    public static float calculateExchange() {
+        float money = moneyToBeExchanged();
+        float rate = getRate();
+        return money * rate;
+    }
 }
