35ce6bc8cc0fcc1f86b976a9f8ca8d0fc723c1cf
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-05-08 13:53:05 +0300

    created an API class and created a getRate() method in it created an Http client and request for the API, plus cleaned up some of the previous code

--- CurrencyExchangeApp.java
+++ CurrencyExchangeApp.java
@@ -1,6 +1,11 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+//import org.json.JSONObject;
 
 public class CurrencyExchangeApp {
     public static void main(String[] args){
@@ -12,29 +17,42 @@ public class CurrencyExchangeApp {
 
 class Exchange {
 
+    private static final Scanner sc = new Scanner(System.in);
+
     LocalDateTime DateTime = LocalDateTime.now();
     DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     String timeStamp = DateTime.format(format);
 
     public static float moneyToBeExchanged() {
-        Scanner sc = new Scanner(System.in);
         System.out.print("please enter the amount of money you want to exchange: ");
-        float money = sc.nextFloat();
-        sc.close();
-        return money;
+        return sc.nextFloat();
     }
         
-    public static float getRate() {
-        Scanner sc = new Scanner(System.in);
+    public static float rateGetter() {
         System.out.print("please enter the current exchange rate: ");
-        Float rate = sc.nextFloat();
-        sc.close();
-        return rate;
+        return sc.nextFloat();
     }
 
     public static float calculateExchange() {
         float money = moneyToBeExchanged();
-        float rate = getRate();
+        float rate = rateGetter();
         return money * rate;
+    }
+}
+
+class API {
+    public static void getRate() {
+        try {
+            String apiKey = "76abdb90da458f91cb518b75";
+            String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
+            
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .build();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
