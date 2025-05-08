f434e3fb7efe15d991020b5089f51ab8648e3d2f
Yad <yad.qadir2005@gmail.com>
2025-05-08 18:46:52 +0300

    Changed calculateExchante() method to calculate the exchange into a float, create the timestamp at the time of calculation, and call the saveExchangeLog() method to save the data into the database. Added saveExchangeLog() method which writes the data to the SQL database table named exchange_log in the database exchange_schema.

--- CurrencyExchangeApp.java
+++ CurrencyExchangeApp.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class CurrencyExchangeApp {
     public static void main(String[] args){
-        //System.out.println(Exchange.calculateExchange());
+        System.out.println(Exchange.calculateExchange());
         //Exchange ex1 = new Exchange();
         //System.out.println(ex1.timeStamp);
     }
@@ -25,7 +25,7 @@ class Exchange {
         Scanner sc = new Scanner(System.in);
         System.out.print("please enter the amount of money you want to exchange: ");
         float money = sc.nextFloat();
-        sc.close();
+        //sc.close(); closing the scanner here was causing issues
         return money;
     }
         
@@ -39,24 +39,53 @@ class Exchange {
     public static float calculateExchange() {
         float money = moneyToBeExchanged();
         float rate = getRate();
-        return money * rate;
+        float result = money * rate;
+
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));// generates the timestamp at the time of the calculation
+
+        database.saveExchangeLog(money, rate, result, timestamp);
+
+        return result;
     }
 }
 class database {
     public static void main(String[] args) {
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/login_schema", "root", "yadbruh1");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/exchange_schema", "root", "yadbruh1");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM exchange_log");
             while(resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
+                System.out.print("ID: ");
+                System.out.println(resultSet.getString("idexchange_log"));
+                System.out.print("Original Amount: ");
+                System.out.println(resultSet.getString("amount"));
+                System.out.print("Exchange rate: ");
+                System.out.println(resultSet.getString("rate"));
+                System.out.print("Resulting Amount: ");
+                System.out.println(resultSet.getString("result"));
+                System.out.print("Time: ");
+                System.out.println(resultSet.getString("timestamp"));
         }
         }catch(SQLException e){
             e.printStackTrace();
         }
         
     }   
-    public static void testStatement(){
-        
+
+    public static void saveExchangeLog(float money, float rate, float result, String timestamp) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/exchange_schema", "root", "yadbruh1");
+            String query = "INSERT INTO exchange_log (amount, rate, result, timestamp) VALUES (?, ?, ?, ?)"; //i dont understand this tbh its black magic
+            java.sql.PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setFloat(1, money);
+            preparedStatement.setFloat(2, rate);
+            preparedStatement.setFloat(3, result);
+            preparedStatement.setString(4, timestamp);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
